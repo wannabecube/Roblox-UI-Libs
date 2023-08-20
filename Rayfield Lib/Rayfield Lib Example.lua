4bc5a6b62a1fcd730ef6e28ffc592666840ccae8
Cubism <92665858+wannabecube@users.noreply.github.com>
2023-08-20 23:51:06 +1000

    Update Rayfield Lib Example.lua

--- Rayfield Lib/Rayfield Lib Example.lua
+++ Rayfield Lib/Rayfield Lib Example.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/wannabecube/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
 	Name = "Rayfield Example Window",
